aee66008db74ddd7a5a67ff7d627753c93499abe
Kevin234r2312 <kevin.cbx12@gmail.com>
2026-01-10 16:45:45 -0300

    Create pix.ts

---
+++ lib/gateways/blupay/pix.ts
@@ -0,0 +1,18 @@
+import { blupayRequest } from './client'
+
+export async function createBluPayPixIn(data: any) {
+  return blupayRequest('/api/v1/transactions', 'POST', {
+    amount: data.amount, // em centavos
+    paymentMethod: 'pix',
+    externalRef: data.externalRef,
+    customer: data.customer,
+    items: data.items ?? [
+      {
+        title: 'Pagamento Klose',
+        unitPrice: data.amount,
+        quantity: 1,
+        tangible: false,
+      },
+    ],
+  })
+}
