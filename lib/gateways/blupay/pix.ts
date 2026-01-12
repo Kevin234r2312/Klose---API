2e848ca18c473653a9e9e0a2747aec13f3280cad
Kevin234r2312 <kevin.cbx12@gmail.com>
2026-01-12 12:10:20 -0300

    Update pix.ts

--- lib/gateways/blupay/pix.ts
+++ lib/gateways/blupay/pix.ts
@@ -1,8 +1,8 @@
 import { blupayRequest } from './client'
 
-export async function createBluPayPixIn(data: any) {
+export async function createPixIn(data: any) {
   return blupayRequest('/api/v1/transactions', 'POST', {
-    amount: data.amount, // em centavos
+    amount: data.amount,
     paymentMethod: 'pix',
     externalRef: data.externalRef,
     customer: data.customer,
